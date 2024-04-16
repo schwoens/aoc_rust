21db6479c94a0814473a96589e6bdbfa0ac70d77
schwoens <schwoens@protonmail.com>
2024-04-17 00:01:39 +0200

    complete 2023 day 8 part 2

--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -1,9 +1,9 @@
-use core::panic;
 use std::collections::HashMap;
 
 fn main() {
     let input = include_str!("../input.txt");
     println!("Part1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
@@ -35,6 +35,56 @@ fn part1(input: &str) -> usize {
     }
 }
 
+fn part2(input: &str) -> usize {
+    let (directions, nodes_str) = input.split_once("\n\n").unwrap();
+    let mut nodes = HashMap::new();
+    let mut current_nodes = Vec::new();
+    for line in nodes_str.lines() {
+        let (label, node) = line.split_once(" = ").unwrap();
+        let node = node.trim_matches(['(', ')']);
+        let (left, right) = node.split_once(", ").unwrap();
+
+        if label.ends_with('A') {
+            current_nodes.push(label);
+        }
+
+        nodes.insert(label, (left, right));
+    }
+
+    let mut arrival_times = Vec::new();
+    'outer: for current_node in current_nodes {
+        let mut current_node = current_node;
+        let mut counter = 0;
+        loop {
+            for direction in directions.chars() {
+                match direction {
+                    'L' => current_node = nodes[current_node].0,
+                    'R' => current_node = nodes[current_node].1,
+                    _ => panic!("illegal direction"),
+                }
+                counter += 1;
+                if current_node.ends_with('Z') {
+                    arrival_times.push(counter);
+                    continue 'outer;
+                }
+            }
+        }
+    }
+    arrival_times.into_iter().reduce(lcm).unwrap()
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -43,5 +93,11 @@ mod test {
     fn part1_works() {
         let test_input = include_str!("../test_input.txt");
         assert_eq!(part1(test_input), 2);
+    }
+
+    #[test]
+    fn part2_works() {
+        let test_input = include_str!("../test_input2.txt");
+        assert_eq!(part2(test_input), 6);
     }
 }
